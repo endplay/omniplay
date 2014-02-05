0b3a45895b13015c23c96a0f795bafae1c4328d8
David Devecsery <ddevec@umich.edu>
2014-02-05 11:32:40 -0500

    tweaks to cvsimport/export scripts

--- cvsimport.sh
+++ cvsimport.sh
@@ -4,11 +4,17 @@ CVSCHECKOUT=~/.gitcvsimport/
 MODULE=omniplay
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
+git stash
+
+git checkout cvs
 
 git cvsimport -k
+
+git checkout $BRANCH
 git merge cvs || {
 	echo "Could not merge cvs"
 	git checkout $BRANCH
 	exit 1
 }
+git stash pop
 
