7aac9f6be92713c9f368692fafdfbfd99a64081b
mcchow <mcchow>
2014-03-02 21:37:12 +0000

    Fixed write/read ranges of linkages from the linkage pin tool

--- logdb/query.py
+++ logdb/query.py
@@ -9,6 +9,9 @@ import sqlite3
 import argparse
 import subprocess
 import collections
+from operator import attrgetter
+
+import pydot
 
 # our modules
 import ipc
@@ -29,8 +32,9 @@ class Query(object):
         self.graph = ipc.IPCGraph()
 
         # list of lists of write infos
-        # Invariant: All the write infos in an element must have the same group id
-        #  represents a node/group id to replay
+        # Invariant: All the write infos in an element must have 
+        #   the same group id
+        #   represents a node/group id to replay
         self.run_queue = []
 
         # timings
@@ -43,7 +47,10 @@ class Query(object):
         self.run_queue.append(write_infos)
         
         # for querying meta information
-        self.rldb = logdb.ReplayLogDB(self.runtime_info.omniplay_location, logdb_name="replay.db", logdb_dir="/replay_logdb", replay_table_name="replays")
+        self.rldb = logdb.ReplayLogDB(self.runtime_info.omniplay_location,
+                                        logdb_name="replay.db",
+                                        logdb_dir="/replay_logdb",
+                                        replay_table_name="replays")
 
         self.linkage_tool = self.find_linkage_tool()
         if not self.linkage_tool:
@@ -67,17 +74,19 @@ class Query(object):
                 linkage_tool_name = "linkage_data.so"
             else:
                 linkage_tool_name = "linkage_copy.so"
+        elif "DATA" in linkages:
+            linkage_tool_name = "linkage_data.so"
         else:
             print("No supported linkages of that type, defaulting to copy")
             linkage_tool_name = "linkage_copy.so"
 
         return linkage_tool_name
 
-    def lookup_sourcing_read(self, read_info):
+    def lookup_sourcing_read(self, group_id, pid, syscall):
         rldb = logdb.ReplayLogDB(self.runtime_info.omniplay_location)
         rldb.init_cursor()
 
-        links = rldb.lookup_writes(read_info)
+        links = rldb.lookup_sourcing_writes(group_id, pid, syscall)
         rldb.close_cursor()
 
         return links
@@ -97,7 +106,8 @@ class Query(object):
             print("Trying to find matching exec for group id %d, pid %d" %
                     (group_id, pid))
 
-        cmd = ''.join([self.runtime_info.omniplay_location, "/test/parseklog ", klog])
+        cmd = ''.join([self.runtime_info.omniplay_location,
+                            "/test/parseklog ", klog])
         if self.runtime_info.verbose:
             print(cmd)
         logproc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
@@ -172,7 +182,7 @@ class Query(object):
 
         prev_write_info = None
         prev_read_info = None
-        links = {}
+        links = collections.defaultdict(list)
 
         q = open(self.query_output, "r")
         for line in q:
@@ -183,53 +193,136 @@ class Query(object):
             # format: TYPE CHANNEL GROUPID RECORDPID SYSCALLCNT OFFSET
             fields = line.split(" ")
             assert len(fields) == 12
-            (write_type, write_channel, write_rec_group, write_pid, write_syscall, write_idx) = \
-                    (fields[0], fields[1], int(fields[2]), int(fields[3]), int(fields[4]), int(fields[5]))
-            (read_type, read_channel, read_rec_group, read_pid, read_syscall, read_idx) = \
-                    (fields[6], fields[7], int(fields[8]), int(fields[9]), int(fields[10]), int(fields[11]))
 
-            if prev_write_info is None:
-                if write_type == "WRITE":
-                    prev_write_info = opinfo.WriteInfo(write_rec_group, write_pid, write_syscall, write_idx, channel=write_channel)
-                elif write_type == "EXEC":
-                    prev_write_info = opinfo.ExecInfo(write_rec_group, write_pid, write_syscall, write_idx, channel=write_channel)
-            elif prev_write_info.group_id == write_rec_group and \
-                        prev_write_info.pid == write_pid and \
-                        prev_write_info.syscall == write_syscall and \
-                        prev_write_info.channel == write_channel and \
-                        __instance_check(prev_write_info, write_type):
-                prev_write_info.size += 1
+            write_type = fields[0]
+            write_channel = fields[1]
+            write_rec_group = int(fields[2])
+            write_pid = int(fields[3])
+            write_syscall = int(fields[4])
+            write_idx = int(fields[5])
+
+            # sourcing read
+            read_type = fields[6]
+            read_channel = fields[7]
+            read_rec_group = int(fields[8])
+            read_pid = int(fields[9])
+            read_syscall = int(fields[10])
+            read_idx = int(fields[11])
+
+            # group together writes, group together reads that point to the same write
+            if write_type == "WRITE":
+                write_info = opinfo.WriteInfo(write_rec_group,
+                                                write_pid,
+                                                write_syscall,
+                                                write_idx,
+                                                channel=write_channel)
+            elif write_type == "EXEC":
+                write_info = opinfo.ExecInfo(write_rec_group,
+                                                write_pid,
+                                                write_syscall,
+                                                write_idx,
+                                                channel=write_channel)
             else:
-                if write_type == "WRITE":
-                    prev_write_info = opinfo.WriteInfo(write_rec_group, write_pid, write_syscall, write_idx, channel=write_channel)
-                elif write_type == "EXEC":
-                    prev_write_info = opinfo.ExecInfo(write_rec_group, write_pid, write_syscall, write_idx, channel=write_channel)
+                print("Unknown write type %s" % write_type)
+                continue
 
-            if prev_read_info is None:
-                if read_type == "READ":
-                    prev_read_info = opinfo.ReadInfo(read_rec_group, read_pid, read_syscall, read_idx, channel=read_type)
-                elif read_type == "EXEC":
-                    prev_read_info = opinfo.ExecInfo(read_rec_group, read_pid, read_syscall, read_idx, channel=read_type)
-            elif prev_read_info.group_id == read_rec_group and \
-                    prev_read_info.pid == read_pid and \
-                    prev_read_info.syscall == read_syscall and \
-                    prev_read_info.channel == read_channel and \
-                    __instance_check(prev_read_info, read_type):
-                 prev_read_info.size += 1
+            if read_type == "READ":
+                read_info = opinfo.ReadInfo(read_rec_group,
+                                                read_pid,
+                                                read_syscall,
+                                                read_idx,
+                                                channel=read_channel)
+            elif read_type == "EXEC":
+                read_info = opinfo.ExecInfo(read_rec_group,
+                                                read_pid,
+                                                read_syscall,
+                                                read_idx,
+                                                channel=read_channel)
             else:
-                if read_type == "READ":
-                    prev_read_info = opinfo.ReadInfo(read_rec_group, read_pid, read_syscall, read_idx, channel=read_type)
-                elif read_type == "EXEC":
-                    prev_read_info = opinfo.ExecInfo(read_rec_group, read_pid, read_syscall, read_idx, channel=read_type)
+                print("Unknown read type %s" % read_type)
+                continue
 
-            # now link the current write info to the current read info, if not exists
-            if prev_write_info and prev_read_info:
-                if prev_write_info not in links:
-                    links[prev_write_info] = prev_read_info
-        q.close()
+            links[write_info].append(read_info)
 
-        # return mappings of writes to reads within this process
         return links
+
+    def visualize_query_output(self, links, writes, output_file):
+        '''
+        Create a dot graph of an intraprocess links, for a given input set of writes
+        '''
+        graph = pydot.Dot(graph_type='digraph', rankdir='LR')
+
+        write2reads = {}
+        # mapping from ReadInfo to pydot Node
+        read_nodes = {}
+
+        # sort the writes
+        writes = sorted(writes, key=attrgetter('group_id', 'pid', 'syscall', 'offset'))
+
+        # go through the links and create the nodes
+        for write in writes:
+            reads = []
+            for i in range(0, write.size):
+                tmp_write = opinfo.WriteInfo(write.group_id, write.pid,
+                                            write.syscall, write.offset + i,
+                                            channel=write.channel,
+                                            size=1)
+                if tmp_write not in links:
+                    continue
+                for r in links[tmp_write]:
+                    reads.append(r)
+
+            reads = opinfo.remove_dups(reads)
+            reads = opinfo.group_infos(reads)
+            write2reads[write] = reads
+
+        # now repartition the writes into ranges that only have the same reads
+        repart_writes = []
+        prev_write = None
+        for write in writes:
+            for i in range(0, write.size):
+                if prev_write is None:
+                    prev_write = opinfo.WriteInfo(write.group_id, write.pid, write.syscall, write.offset,
+                                                    channel=write.channel, size=1)
+                    repart_writes.append(prev_write)
+                else:
+                    # check to see if the write is next continuously and if they have the same sourcing reads
+                    if (write.offset == (prev_write.offset + prev_write.size)) and \
+                            opinfo.compare_lists(write2reads[write], write2reads[prev_write]):
+                        prev_write.size += 1 
+                    else:
+                        prev_write = opinfo.WriteInfo(write.group_id, write.pid, write.syscall, write.offset,
+                                                    channel=write.channel, size=1)
+                        repart_writes.append(prev_write)
+
+        if self.runtime_info.verbose:
+            for write in repart_writes:
+                print("%s" % write)
+
+        for write in repart_writes:
+            write_name = 'WRITE \n'
+            write_name += ('Group %d, Pid %d, Syscall %d, Offset %d, size %d' % (
+                    write.group_id, write.pid, write.syscall, write.offset, write.size))
+            write_name += ('\n%s' % write.channel)
+            write_node = pydot.Node(write_name, shape="box")
+            graph.add_node(write_node)
+
+            for r in reads:
+                if (r, r.size) not in read_nodes:
+                    read_name = 'READ\n'
+                    read_name += ('Group %d, Pid %d, Syscall %d, Offset %d, size %d' % (
+                        r.group_id, r.pid, r.syscall, r.offset, r.size))
+                    read_name += ('\n%s' % r.channel)
+                    read_node = pydot.Node(read_name, shape="box")
+                    read_nodes[(r, r.size)] = read_node
+                read_node = read_nodes[(r, r.size)]
+                graph.add_node(read_node)
+                edge = pydot.Edge(read_node, write_node)
+                graph.add_edge(edge)
+
+        graph.write(output_file)
+
+        print("Done writing intraprocess graph!")
 
     def run(self):
         '''
@@ -257,7 +350,9 @@ class Query(object):
             # os.remove(self.query_output)
 
             replay_dir = "/replay_logdb/rec_" + str(group_id)
-            replay_process = self.runtime_info.replay(replay_dir, self.query_log, pin=True)
+            replay_process = self.runtime_info.replay(replay_dir,
+                                                        self.query_log,
+                                                        pin=True)
             self.current_pid = replay_process.pid
             self.current_gid = group_id
 
@@ -265,7 +360,10 @@ class Query(object):
             time.sleep(1)
 
             self.tool_start = int(time.time())
-            attach_process = self.runtime_info.attach_tool(replay_process.pid, self.linkage_tool, self.query_output, self.query_log)
+            attach_process = self.runtime_info.attach_tool(replay_process.pid,
+                                                            self.linkage_tool,
+                                                            self.query_output,
+                                                            self.query_log)
             attach_process.wait()
             replay_process.wait()
             self.tool_end = int(time.time())
@@ -277,20 +375,77 @@ class Query(object):
             #     print("You might want to look for errors in %s" % self.query_log_filename)
             #     return False
 
+            # TODO: might want to make a copy of the output somewhere for debugging purposes
+
             new_writes = []
             # Extra post-processing step for interpreting tokens
             self.interpret_query_output()
             # find intraprocess links
             links = self.parse_query_output()
-            # For every write that we are interested in, look up the write to the sourcing read
+            # For every write that we are interested in, 
+            #   look up the write to the sourcing read
             for wi in wis: 
-                if wi not in links:
+
+                # reads associated with sourcing this write range
+                sourcing_reads = []
+                for i in range(0, wi.size):
+                    tmp_write = opinfo.WriteInfo(wi.group_id, wi.pid,
+                                                    wi.syscall,
+                                                    wi.offset + i,
+                                                    channel=wi.channel,
+                                                    size=1)
+                    if tmp_write not in links:
+                        continue
+                    # add reads from intraprocess links
+                    for r in links[tmp_write]:
+                        sourcing_reads.append(r)
+
+                if not sourcing_reads:
+                    if self.runtime_info.verbose:
+                        print("No info found for: %s" % str(wi))
                     continue
-                # internal process links
-                ri = links[wi]
-                # now find the IPC links for this read
+
+                sourcing_reads = opinfo.remove_dups(sourcing_reads)
+                sourcing_reads = opinfo.group_infos(sourcing_reads)
+                # now find the IPC links for each read group
+                for ri in sourcing_reads:
+                    if (isinstance(ri, opinfo.ReadInfo)):
+                        # query the DB
+                        ipc_links = self.lookup_sourcing_read(ri.group_id, ri.pid, ri.syscall)
+                        if self.runtime_info.verbose:
+                            print("Found %d IPC writes for read %s" % 
+                                    (len(ipc_links), str(ri)))
+
+                        # create a node in the IPC graph for the process that
+                        #  produced the read if we haven't created it yet
+                        if not self.graph.has_node(ri.group_id, ri.pid):
+                            program_cmd = self.rldb.get_program_args(ri.group_id)
+                            self.graph.add_node(ri.group_id, ri.pid, cmd=program_cmd)
+                            if self.runtime_info.verbose:
+                                print("Add read node (%d, %d)" % (ri.group_id, ri.pid))
+
+                        read_node = self.graph.get_node(ri.group_id, ri.pid)
+                        assert (read_node)
+                        read_node.add_read(ri)
+
+                        for (r, write) in ipc_links.iteritems():
+                            if not self.graph.has_node(write.group_id, write.pid):
+                                program_cmd = self.rldb.get_program_args(write.group_id)
+                                self.graph.add_node(write.group_id, write.pid, cmd=program_cmd)
+                                new_writes.append(write)
+                            write_node = self.graph.get_node(write.group_id, write.pid)
+                            assert(write_node)
+                            write_node.add_write(write)
+                            if self.runtime_info.verbose:
+                                print("Add write node (%d, %d)" % (write.group_id, write.pid))
+                            # add an edge from this write to the read
+                            write_node.add_edge(read_node, write, r)
+
+
+
                 if (isinstance(ri, opinfo.ReadInfo)):
-                    writes = self.lookup_sourcing_read(ri)
+                    # query the DB
+                    writes = self.lookup_sourcing_read(ri.group_id, ri.pid, ri.syscall)
                     if self.runtime_info.verbose:
                         print("Found %d writes for read %s" % (len(writes), str(ri)))
 
@@ -322,7 +477,8 @@ class Query(object):
                         continue
                     assert parent_id != wi.group_id
                     if self.runtime_info.verbose:
-                        print("Match exec in group %d, pid %d from parent %d" % (ri.group_id, ri.pid, parent_id))
+                        print("Match exec in group %d, pid %d from parent %d" % 
+                                (ri.group_id, ri.pid, parent_id))
                     exec_info = self.find_matching_exec(parent_id, ri.pid)
                     if not exec_info:
                         if self.runtime_info.verbose:
@@ -332,7 +488,8 @@ class Query(object):
                         program_cmd = self.rldb.get_program_args(exec_info.group_id)
                         if not program_cmd:
                             if self.runtime_info.verbose:
-                                print("Could not find cmd for group %d, pid %d" % (exec_info.group_id, exec_info.pid))
+                                print("Could not find cmd for group %d, pid %d" % 
+                                        (exec_info.group_id, exec_info.pid))
                             program_cmd = ''
                         self.graph.add_node(exec_info.group_id, exec_info.pid, cmd=program_cmd)
                         new_writes.append(exec_info)
