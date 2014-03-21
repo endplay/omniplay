29bbc1f24c7971a55245ff8b32782292689cd2c6
ddevec <ddevec>
2014-03-21 21:38:32 +0000

    Updated launcher.c

--- test/launcher.c
+++ test/launcher.c
@@ -28,112 +28,112 @@ int main (int argc, char* argv[])
 	int save_mmap = 0;
 
 	struct option long_options[] = {
-	{"logdir", required_argument, 0, 0},
-	{"pthread", required_argument, 0, 0},
-	{0, 0, 0, 0}
+		{"logdir", required_argument, 0, 0},
+		{"pthread", required_argument, 0, 0},
+		{0, 0, 0, 0}
 	};
 
 	/*
 	for (i = 0; i < argc; i++) {
-	printf("Got input arg of %s\n", argv[i]);
+		printf("Got input arg of %s\n", argv[i]);
 	}
 	*/
 
 	while (1) {
-	char opt;
-	int option_index = 0;
+		char opt;
+		int option_index = 0;
 
-	setenv("POSIXLY_CORRECT", "1", 1);
-	opt = getopt_long(argc, argv, "mh", long_options, &option_index);
-	unsetenv("POSIXLY_CORRECT");
-	//printf("getopt_long returns %c (%d)\n", opt, opt);
+		setenv("POSIXLY_CORRECT", "1", 1);
+		opt = getopt_long(argc, argv, "mh", long_options, &option_index);
+		unsetenv("POSIXLY_CORRECT");
+		//printf("getopt_long returns %c (%d)\n", opt, opt);
 
-	if (opt == -1) {
-		break;
-	}
-
-	switch(opt) {
-		case 0:
-		switch(option_index) {
-			case 0:
-			//printf("logdir is %s\n", optarg);
-			logdir = optarg;
-			assert(optarg != NULL);
+		if (opt == -1) {
 			break;
-			case 1:
-			//printf("pthread libdir is %s\n", optarg);
-			libdir = optarg;
-			break;
-			default:
-			assert(0);
 		}
-		break;
-		case 'm':
-		//printf("save_mmap is on");
-		save_mmap = 1;
-		break;
-		case 'h':
-		format();
-		break;
-		default:
-		fprintf(stderr, "Unrecognized option\n");
-		format();
-		break;
-	}
+
+		switch(opt) {
+			case 0:
+				switch(option_index) {
+					case 0:
+						//printf("logdir is %s\n", optarg);
+						logdir = optarg;
+						assert(optarg != NULL);
+						break;
+					case 1:
+						//printf("pthread libdir is %s\n", optarg);
+						libdir = optarg;
+						break;
+					default:
+						assert(0);
+				}
+				break;
+			case 'm':
+				//printf("save_mmap is on");
+				save_mmap = 1;
+				break;
+			case 'h':
+				format();
+				break;
+			default:
+				fprintf(stderr, "Unrecognized option\n");
+				format();
+				break;
+		}
 	}
 	base = optind;
 
 	/* David D. Replaced with proper getopts */
 	/*
 	for (base = 1; base < argc; base++) {
-	if (argc > base+1 && !strncmp(argv[base], "--pthread", 8)) {
-		libdir = argv[base+1];
-		base++;
-	} else if (argc > base+1 && !strncmp(argv[base], "--logdir", 8)) {
-		logdir = argv[base+1];
-		base++;
-	} else if (!strncmp(argv[base], "--link-debug", 8)) {
-		link_debug = 1;
-	} else if (!strncmp(argv[base], "-m", 2)) {
-		save_mmap = 1;
-	} else {
-		break; // unrecognized arg - should be logdir
-	}
+		if (argc > base+1 && !strncmp(argv[base], "--pthread", 8)) {
+			libdir = argv[base+1];
+			base++;
+		} else if (argc > base+1 && !strncmp(argv[base], "--logdir", 8)) {
+			logdir = argv[base+1];
+			base++;
+		} else if (!strncmp(argv[base], "--link-debug", 8)) {
+			link_debug = 1;
+		} else if (!strncmp(argv[base], "-m", 2)) {
+			save_mmap = 1;
+		} else {
+			break; // unrecognized arg - should be logdir
+		}
 	}
 	*/
 
 
 	if (argc-base < 1) {
-	fprintf(stderr, "Program name not specified");
-	format();
+		fprintf(stderr, "Program name not specified");
+		format();
 	}
 
 	/*
 	for (i = base; i < argc; i++) {
-	printf("Got non-opt arg: %s\n", argv[i]);
+		printf("Got non-opt arg: %s\n", argv[i]);
 	}
 	*/
 
 	fd = open ("/dev/spec0", O_RDWR);
 	if (fd < 0) {
-	perror ("open /dev/spec0");
-	return -1;
+		perror ("open /dev/spec0");
+		return -1;
 	}
 
 	if (libdir) { 
-	strcpy (ldpath, libdir);
-	for (i = 0; i < strlen(ldpath); i++) {
-		if (ldpath[i] == ':') {
-		ldpath[i] = '\0';
-		break;
+		strcpy (ldpath, libdir);
+		for (i = 0; i < strlen(ldpath); i++) {
+			if (ldpath[i] == ':') {
+				ldpath[i] = '\0';
+				break;
+			}
 		}
-	}
-	strcat (ldpath, "/");
-	strcat (ldpath, "ld-linux.so.2");
-	argv[base-1] = ldpath;
-	linkpath = ldpath;
+		strcat (ldpath, "/");
+		strcat (ldpath, "ld-linux.so.2");
+		argv[base-1] = ldpath;
+		linkpath = ldpath;
 
-	setenv("LD_LIBRARY_PATH", libdir, 1);
+		setenv("LD_LIBRARY_PATH", libdir, 1);
 	}
 	if (link_debug) setenv("LD_DEBUG", "libs", 1);
 
@@ -143,4 +143,5 @@ int main (int argc, char* argv[])
 	// replay_fork should never return if it succeeds
 	fprintf (stderr, "replay_fork failed, rc = %d\n", rc);
 	return rc;
+
 }
