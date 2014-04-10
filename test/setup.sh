49fa05492313ad6298055e21b38ba19e62a1833d
David Devecsery <ddevec@umich.edu>
2014-04-10 17:45:02 +0000

    Added script to easily launch record processes, added proc entry that returns if current entry is a record process or not (for bash ps1 notification)

--- test/setup.sh
+++ test/setup.sh
@@ -1,27 +1,38 @@
-#/bin/sh
+#!/bin/bash
 
-if [ ! -e /replay_cache ]
-then
-    mkdir /replay_cache
-    chmod a+rwx /replay_cache
+if [[ -z $OMNIPLAY_DIR ]]; then
+	echo "OMNIPLAY_DIR not set, please run <omniplay_dir>/scripts/setup.sh"
+	exit 0
 fi
 
-if [ ! -e /replay_logdb ]
-then
-    mkdir /replay_logdb
-    chmod a+rwx /replay_logdb
-fi
+source $OMNIPLAY_DIR/scripts/common.sh
 
-/sbin/insmod dev/spec.ko || {
-	echo "Unable to insert spec!"
-	exit 1
-}
+setup_spec
 
-if [ ! -e /dev/spec0 ]
-then
-    mknod /dev/spec0 c 149 0
-		chmod 777 /dev/spec0
-fi
-
-# To allow PIN to attach
-echo 0 > /proc/sys/kernel/yama/ptrace_scope
+##/bin/sh
+#
+#if [ ! -e /replay_cache ]
+#then
+#    mkdir /replay_cache
+#    chmod a+rwx /replay_cache
+#fi
+#
+#if [ ! -e /replay_logdb ]
+#then
+#    mkdir /replay_logdb
+#    chmod a+rwx /replay_logdb
+#fi
+#
+#/sbin/insmod dev/spec.ko || {
+#	echo "Unable to insert spec!"
+#	exit 1
+#}
+##
+#if [ ! -e /dev/spec0 ]
+#then
+#    mknod /dev/spec0 c 149 0
+#		chmod 777 /dev/spec0
+#fi
+#
+## To allow PIN to attach
+#echo 0 > /proc/sys/kernel/yama/ptrace_scope
