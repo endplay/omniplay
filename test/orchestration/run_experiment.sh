8ccf28797c36a7e566f989a48426eabe75d31b68
Andrew Quinn <arquinn@umich.edu>
2016-01-19 11:49:04 -0500

    whoops, forgot these files

---
+++ test/orchestration/run_experiment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+BASE="$OMNIPLAY_DIR/test/experiment_config_files"
+HOST=".$1"
+NUM_HOSTS="16"
+
+GS=$BASE"/gzip.rec.2"
+GS_NAME="ghostscript"
+
+GZIP=$BASE"/gzip.rec.52"
+GZIP_NAME="gzip"
+
+EVINCE=$BASE"/evince.rec.61441"
+EVINCE_NAME="evince"
+
+NGINX=$BASE"/mine.rec.73731"
+NGINX_NAME="nginx"
+
+TEST=$EVINCE
+NAME=$EVINCE_NAME
+OUTPUT="emulab_output_$NAME"
+
+
+echo -n "Password:"
+read -s password
+
+
+python emulab_experiment.py $TEST/experiment.config -o $OUTPUT -p $TEST/prefix.tar.gz --hs=$HOST -n $NUM_HOSTS -c $TEST/seqtt.results -s --password $password
+python emulab_experiment.py $TEST/experiment.config -o $OUTPUT --hs=$HOST -c $TEST/seqtt.results -n $NUM_HOSTS --password $password
